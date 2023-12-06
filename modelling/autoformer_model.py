31bb64211295af4164279b69b4123fe113ace28a
LucaMancaITA <luca-manca@outlook.it>
2023-12-06 22:26:09 +0100

    autoformer architecture

---
+++ modelling/autoformer_model.py
@@ -0,0 +1,57 @@
+
+# Import modules
+import torch
+
+from modelling.base_model import BaseModel
+from net.autoformer.autoformer import Autoformer
+
+
+class AutoformerModel(BaseModel):
+    """Autoformer model."""
+    def __init__(self, data_args, train_eval_args, informer_args):
+        super().__init__(data_args, train_eval_args)
+        self.model_args = informer_args
+
+        self.model = self.build_model()
+
+    def build_model(self):
+        model = Autoformer(
+            enc_in=self.model_args.enc_in,
+            dec_in=self.model_args.dec_in,
+            c_out=self.model_args.c_out,
+            seq_len=self.data_args.input_len,
+            label_len=self.data_args.label_len,
+            pred_len=self.data_args.pred_len,
+            d_model=self.model_args.d_model,
+            n_heads=self.model_args.n_heads,
+            e_layers=self.model_args.e_layers,
+            d_layers=self.model_args.d_layers,
+            d_ff=self.model_args.d_ff
+        )
+
+        num_params = self.count_parameters(model)
+        print(f"Number of parameters: {num_params}")
+
+        return model
+
+    def process_one_batch(self, batch_x, batch_y, batch_x_mark, batch_y_mark):
+        batch_x = batch_x.float()
+        batch_y = batch_y.float()
+
+        batch_x_mark = batch_x_mark.float()
+        batch_y_mark = batch_y_mark.float()
+
+        # decoder input
+        dec_inp = torch.zeros(
+            [batch_y.shape[0], self.data_args.pred_len, batch_y.shape[-1]]
+        ).float()
+        dec_inp = torch.cat(
+            [batch_y[:,:self.data_args.label_len,:], dec_inp], dim=1
+        ).float()
+        # encoder - decoder
+        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+
+        f_dim = 0
+        batch_y = batch_y[:,-self.data_args.pred_len:,f_dim:]
+
+        return outputs, batch_y
--- net/informer/informer.py
+++ net/informer/informer.py
@@ -5,17 +5,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from utils.masking import TriangularCausalMask, ProbMask
-from net.informer.encoder import Encoder, EncoderLayer, ConvLayer, EncoderStack
-from net.informer.decoder import Decoder, DecoderLayer
-from net.informer.attn import FullAttention, ProbAttention, AttentionLayer
-from net.informer.embed import DataEmbedding
+from net.utils.encoder import Encoder, EncoderLayer, ConvLayer, EncoderStack
+from net.utils.decoder import Decoder, DecoderLayer
+from net.utils.attn import FullAttention, ProbAttention, AttentionLayer
+from net.utils.embed import DataEmbedding
 
 
 class Informer(nn.Module):
     def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len,
                 factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512,
                 dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu',
-                output_attention = False, distil=True, mix=True,
+                output_attention=False, distil=True, mix=True,
                 device=torch.device('cuda:0')):
         super(Informer, self).__init__()
         self.pred_len = out_len
